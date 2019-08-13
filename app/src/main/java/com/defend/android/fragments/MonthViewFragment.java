072df637df401e1171f3df7c84d9f5fb9bcffcb1
Sina ZandKarimi <sinazandkarimi@gmail.com>
2019-08-13 14:11:58 +0430

    Fix bugs

--- app/src/main/java/com/defend/android/fragments/MonthViewFragment.java
+++ app/src/main/java/com/defend/android/fragments/MonthViewFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -121,7 +122,7 @@ public class MonthViewFragment extends Fragment {
     public ArrayList<Event> findEventsByDate(int d) {
         ArrayList<Event> dayEvents = new ArrayList<>();
         for(int i = 0;i < events.size();i++) {
-            if(events.get(i).getDay() == d) {
+            if(events.get(i).getDay() == d && events.get(i).getMonth() == month && events.get(i).getYear() == year) {
                 dayEvents.add(events.get(i));
             }
         }
@@ -148,6 +149,7 @@ public class MonthViewFragment extends Fragment {
     }
 
     public void setSelectedDay(int day) {
+        Log.i("salam", "selcted = " + day + " size = " + findEventsByDate(day));
         adapter.setDay(day);
         synchronized(adapter) {
             adapter.notifyDataSetChanged();
